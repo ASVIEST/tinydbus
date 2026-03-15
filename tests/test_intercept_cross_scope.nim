2105a03c1ef9d99a2b5c7c5306eb3dce9788c865
ASVIEST <aaaaaabbbbbbccscc@gmail.com>
2026-03-15 14:36:55 +0300

    Added a test to check if the compiler has a regression when working with symbols.

---
+++ tests/test_intercept_cross_scope.nim
@@ -0,0 +1,54 @@
+import pkg/tinydbus
+import pkg/unittest2
+
+suite "intercept cross-scope":
+  test "cross-scope gensym call":
+    proc fakeHandler(msg: Message): Message =
+      result = initMethodReturnMsg(msg)
+      var body = initBodyBuilder()
+      body.add 100'u32
+      result.setBody(body)
+
+    block scope1:
+      addIntercept("org.test.CrossScope",
+                  "/org/test/Obj",
+                  "org.test.Iface",
+                  "Method",
+                  fakeHandler)
+
+      let msg = initMethodCallMsg(
+        "org.test.CrossScope",
+        "/org/test/Obj",
+        "org.test.Iface",
+        "Method")
+      msg.serial = 1
+      msg.sender = ":1.0"
+
+      let conn: BusConnection = nil
+      let reply = conn.call(msg)
+
+      check reply.kind == mtMethodReturn
+      var br = initBodyReader(reply.body, reply.signature)
+      check br.read[:uint32]() == 100'u32
+
+    block scope2:
+      # Reuses the proc generated in another lexical scope.
+      # But it's not a problem because direct usage of sym
+      # doesn't perform lookup so it doesn't care about
+      # lexical scope bounds.
+
+      let msg = initMethodCallMsg(
+        "org.test.CrossScope",
+        "/org/test/Obj",
+        "org.test.Iface",
+        "Method")
+      msg.serial = 2
+      msg.sender = ":1.0"
+
+      let conn: BusConnection = nil
+      let reply = conn.call(msg)
+
+      check reply.kind == mtMethodReturn
+      var br = initBodyReader(reply.body, reply.signature)
+      check br.read[:uint32]() == 100'u32
+
